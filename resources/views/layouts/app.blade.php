2275f9e909331c21af4752ab48b5660b918c900e
Magaly Diaz <magaly.diaz@enlight.mx>
2018-12-08 21:22:28 -0600

    *Se cambia a asset('img/logo_header.png') la carga del logo Enlight en app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
-                    <img alt="Enlight" src="../img/logo_header.png">
+                    <img alt="Enlight" src="{{ asset('img/logo_header.png') }}">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
