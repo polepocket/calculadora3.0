d4eaa55a81c3fb513309557a4a88573c27d8f870
Magaly Diaz <magaly.diaz@enlight.mx>
2018-12-12 16:53:46 -0600

    Agrega línea en app.blade para verificar compatibilidad de calculadora en IE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Script for polyfilling Promises on IE9 and 10 -->
     <script src='https://cdn.polyfill.io/v2/polyfill.min.js'></script>
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
