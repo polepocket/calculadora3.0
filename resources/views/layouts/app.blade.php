4dbcce1278e0122cf3f6db31ed1e284c7d5d8367
Magaly Diaz <magaly.diaz@enlight.mx>
2018-12-11 19:54:10 -0600

    Se agrega https://cdn.polyfill.io/v2/polyfill.min.js en el encabezado de app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
+    <!-- Script for polyfilling Promises on IE9 and 10 -->
+    <script src='https://cdn.polyfill.io/v2/polyfill.min.js'></script>
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
